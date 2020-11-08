4d9f16c122ddd825a4abca6af4c721c2435b2e9c
Trevor Harmon <trevor.harmon@shopify.com>
2020-11-08 15:37:55 -0500

    Drag and drop even better

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
 import { Input, Box, Text, Button } from "theme-ui";
@@ -20,7 +20,7 @@ export function FileInput({
 
         if (file != null && file.name != null) {
           const filePath = file.path != null ? file.path : file.name;
-          setMediaSrc(URL.createObjectURL(file));
+          setMediaSrc({ file: URL.createObjectURL(file), name: file.name });
           onChange(filePath);
         }
       }
@@ -41,6 +41,7 @@ export function FileInput({
     type: "file",
     multiple: "false",
     ref: fileRef,
+    acceptedFileTypes,
   };
 
   function clearSelection() {
@@ -48,66 +49,75 @@ export function FileInput({
     onChange(null);
   }
 
+  const pendingFileMarkup = (
+    <Text
+      sx={{
+        textAlign: "center",
+        fontSize: 2,
+        fontWeight: "bold",
+        zIndex: 0,
+        position: "relative",
+        background: "white",
+      }}
+    >
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag a video or image here</p>
+      )}
+    </Text>
+  );
+
+  const activeFileMarkup = (
+    <React.Fragment>
+      <video
+        style={{ objectFit: "cover" }}
+        width="100%"
+        height="320"
+        autoPlay
+        loop
+        muted
+        sx={{ m: 0, p: 0, mb: -2 }}
+      >
+        <source src={mediaSrc ? mediaSrc.file : null} type="video/mp4" />
+      </video>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          p: 2,
+        }}
+      >
+        <Text
+          sx={{ background: "rgba(0,0,0,0.5)", color: "white", py: 2, px: 3 }}
+        >
+          {mediaSrc ? mediaSrc.name : null}
+        </Text>
+        <Button onClick={clearSelection}>Clear</Button>
+      </Box>
+    </React.Fragment>
+  );
+
   return (
     <Box
       {...rootProps}
       sx={{
-        borderWidth: "1px",
-        borderStyle: isDragActive ? "dashed" : "solid",
-        borderRadius: "4px",
-        py: mediaSrc != null ? 0 : 5,
         overflow: "hidden",
         position: "relative",
+        borderWidth: "1px",
+        borderRadius: "4px",
+        borderStyle: isDragActive ? "dashed" : "solid",
+        py: mediaSrc != null ? 0 : 5,
       }}
     >
-      {mediaSrc == null ? (
-        <Text
-          sx={{
-            textAlign: "center",
-            fontSize: 2,
-            fontWeight: "bold",
-            zIndex: 0,
-            position: "relative",
-            background: "white",
-          }}
-        >
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <p>Drag a video or image here</p>
-          )}
-        </Text>
-      ) : null}
+      {mediaSrc == null ? pendingFileMarkup : activeFileMarkup}
 
       <Input {...inputProps} sx={{ p: 0, mt: 0, mb: 0 }} />
-
-      {mediaSrc != null ? (
-        <video
-          style={{ objectFit: "cover" }}
-          width="100%"
-          height="320"
-          autoPlay
-          loop
-          muted
-          sx={{ m: 0, p: 0, mb: -2 }}
-        >
-          <source src={mediaSrc} type="video/mp4" />
-        </video>
-      ) : null}
-
-      {mediaSrc != null ? (
-        <Button
-          onClick={clearSelection}
-          sx={{
-            position: "absolute",
-            zIndex: 3,
-            bottom: 2,
-            right: 2,
-          }}
-        >
-          Clear
-        </Button>
-      ) : null}
     </Box>
   );
 }
